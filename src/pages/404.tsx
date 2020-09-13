95ffd984e67b91fd84bec2fdf8a50323dc400090
Leonardo Rocha <lerooks.nick@gmail.com>
2020-09-12 22:07:55 -0300

    chore: cleanup starter stuff

---
+++ src/pages/404.tsx
@@ -0,0 +1,10 @@
+import { PageProps } from 'gatsby'
+import React, { ReactElement } from 'react'
+
+export default function Page404({ data }: PageProps): ReactElement {
+  return (
+    <div>
+      <h1> 404 </h1>
+    </div>
+  )
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import React, { ReactElement } from 'react'
+import { PageProps } from 'gatsby'
+
+export default function IndexPage({ data }: PageProps): ReactElement {
+  return (
+    <div>
+      <h1> Index Page </h1>
+    </div>
+  )
+}
