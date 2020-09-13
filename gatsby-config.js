f31665f3803bfef09c1e15538a94bb42b70a93ee
Leonardo Rocha <lerooks.nick@gmail.com>
2020-09-12 23:18:16 -0300

    chore: add styled components

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,20 +2,21 @@ module.exports = {
   siteMetadata: {
     title: '',
     description: '',
-    author: ''
+    author: '',
   },
   plugins: [
+    `gatsby-plugin-styled-components`,
     'gatsby-plugin-react-helmet',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
         name: 'images',
-        path: `${__dirname}/src/images`
-      }
+        path: `${__dirname}/src/images`,
+      },
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     'gatsby-plugin-typescript',
-    'gatsby-plugin-tsconfig-paths'
-  ]
+    'gatsby-plugin-tsconfig-paths',
+  ],
 }
