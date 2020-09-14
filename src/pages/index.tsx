c4b2092a8c917e6604ff4f4e59aa9e86b5a61a76
Leonardo Rocha <lerooks.nick@gmail.com>
2020-09-14 13:08:32 -0300

    feat: seo component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactElement } from 'react'
 import BaseLayout from 'layouts/base-layout'
+import SEO from 'components/common/seo'
 import Banner from 'components/home/banner'
 import Process from 'components/home/process'
 import Charges from 'components/home/charges'
@@ -7,11 +8,14 @@ import CTA from 'components/home/cta'
 
 export default function IndexPage(): ReactElement {
   return (
-    <BaseLayout>
-      <Banner></Banner>
-      <Process></Process>
-      <Charges></Charges>
-      <CTA></CTA>
-    </BaseLayout>
+    <>
+      <SEO></SEO>
+      <BaseLayout>
+        <Banner></Banner>
+        <Process></Process>
+        <Charges></Charges>
+        <CTA></CTA>
+      </BaseLayout>
+    </>
   )
 }
